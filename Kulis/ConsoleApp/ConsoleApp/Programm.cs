337e5c53e42d62261437c09e2c422abf9a6422d6
kaskader <kulisk126@gmail.com>
2026-04-15 08:49:20 +0200

    read+write mit path

---
+++ Kulis/ConsoleApp/ConsoleApp/Programm.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace ConsoleApp
+{
+    public class Programm
+    {
+        public static string filePath = "C:/Users/User/Documents/Schule/CHC/CHC";
+        public static ReadWrite _rw = new ReadWrite();
+        public static void Main()
+        {
+            _rw.WriteIntoFile(filePath,"test.txt", "test1");
+            Console.WriteLine(_rw.ReadFromFile(filePath, "test.txt"));
+        }
+
+        
+
+        
+    }
+}
---
+++ Kulis/ConsoleApp/ConsoleApp/ReadWrite.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp
+{
+    public class ReadWrite
+    {
+        public string ReadFromFile(string path, string fileName)
+        {
+            string fullPath = Path.Combine(path, fileName);
+
+            if (!File.Exists(fullPath))
+                throw new FileNotFoundException("Datei nicht gefunden", fullPath);
+
+            return File.ReadAllText(fullPath);
+        }
+
+        public void WriteIntoFile(string path, string fileName, string content)
+        {
+            string fullPath = Path.Combine(path, fileName);
+
+            // Ordner erstellen, falls er nicht existiert
+            Directory.CreateDirectory(path);
+
+            File.WriteAllText(fullPath, content);
+        }
+    }
+}
